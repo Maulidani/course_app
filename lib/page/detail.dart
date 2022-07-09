0fa512666ed434a6ad16d55ab0c5787c7ae9f257
maulidani <maulidanimahmud@gmail.com>
2022-07-09 20:21:39 +0800

    update

--- lib/page/detail.dart
+++ lib/page/detail.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import '../util/assets.dart';
 
-class Detail extends StatelessWidget {
+class Detail extends StatefulWidget {
   final Map map;
   const Detail({Key? key, required this.map}) : super(key: key);
 
+  @override
+  State<Detail> createState() => _DetailState();
+}
+
+class _DetailState extends State<Detail> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,7 +17,7 @@ class Detail extends StatelessWidget {
         child: Stack(
           children: [
             Image.asset(
-              map["image"],
+              widget.map["image"],
               fit: BoxFit.cover,
               height: 300,
               width: MediaQuery.of(context).size.width,
@@ -60,7 +65,7 @@ class Detail extends StatelessWidget {
   }
 
   Widget course(){
-    var assetCourse = Assets.courses.where((element) => element.type.contains(map["title"])).toList();
+    var assetCourse = Assets.courses.where((element) => element.type.contains(widget.map["title"])).toList();
     return ListView.builder(
         shrinkWrap: true,
         physics: NeverScrollableScrollPhysics(),
@@ -68,79 +73,90 @@ class Detail extends StatelessWidget {
         padding: EdgeInsets.all(24),
         itemBuilder: (context, index) {
           var course = assetCourse[index];
-          return  Container(
-            margin: EdgeInsets.fromLTRB(0, 0, 0, 6),
-            decoration: BoxDecoration(
-              color: Colors.indigo,
-              boxShadow: [
-                BoxShadow(
-                    offset: Offset(0, 0),
-                    blurRadius: 10,
-                    color: Colors.black12),
-              ],
-              borderRadius: BorderRadius.circular(12),
-            ),
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(16),
-                    child: Image.asset(
-                      course.image,
-                      fit: BoxFit.cover,
-                      height: 70,
-                      width: 70,
+          return  GestureDetector(
+            onTap: () {
+              Scaffold.of(context).showSnackBar(SnackBar(
+                content: Text("Course "+course.name),
+              ));
+            },
+            child : Container(
+              margin: EdgeInsets.fromLTRB(0, 0, 0, 6),
+              decoration: BoxDecoration(
+                color: Colors.indigo,
+                boxShadow: [
+                  BoxShadow(
+                      offset: Offset(0, 0),
+                      blurRadius: 10,
+                      color: Colors.black12),
+                ],
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(16),
+                      child: Image.asset(
+                        course.image,
+                        fit: BoxFit.cover,
+                        height: 70,
+                        width: 70,
+                      ),
                     ),
                   ),
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 8.0, top: 6),
-                        child: Text(
-                          course.name,
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8.0, top: 6),
+                          child: Text(
+                            course.name,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold),
+                          ),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 8.0, top: 8),
-                        child: Text(
-                          course.duration,
-                          style: TextStyle(
-                            color: Colors.white54,
-                            fontSize: 14,
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8.0, top: 8),
+                          child: Text(
+                            course.duration,
+                            style: TextStyle(
+                              color: Colors.white54,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Material(
+                      borderRadius: BorderRadius.circular(100),
+                      child: InkWell(
+                        borderRadius: BorderRadius.circular(100),
+                        onTap: () {
+                          Scaffold.of(context).showSnackBar(SnackBar(
+                            content: Text("Play Course"+course.name),
+                          ));
+                        },
+                        child: Padding(
+                          padding: EdgeInsets.all(6),
+                          child: Icon(
+                            Icons.play_arrow_rounded,
+                            size: 32,
+                            color: Colors.indigo,
                           ),
                         ),
                       ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: Material(
-                    borderRadius: BorderRadius.circular(100),
-                    child: InkWell(
-                      borderRadius: BorderRadius.circular(100),
-                      onTap: () {},
-                      child: Padding(
-                        padding: EdgeInsets.all(6),
-                        child: Icon(
-                          Icons.play_arrow_rounded,
-                          size: 32,
-                          color: Colors.indigo,
-                        ),
-                      ),
                     ),
-                  ),
-                )
-              ],
-            ),
+                  )
+                ],
+              ),
+            )
           );
         });
   }
